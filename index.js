93f7f77f2f866e59506f196c9fc70497f6ed3221
Michał Bednarz <mibed90@gmail.com>
2020-01-16 11:52:17 +0100

    Fetch comments

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github'); // deps from toolkit or marketplace
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+(async () => {
+	try {
+	  const commentsUrl = github.context.payload.pull_request.links.review_comments.href
+	  const commentsEndpoint = commentsUrl.replace('https://api.github.com', '')
+	  console.log(commentsEndpoint)
 
+	// auth like this?
+	  const octokit = new github.GitHub(github.token, {
+	  	previews: ['comfort-fade-preview']
+	  });
 
-  // fetch review thread comments
-  // github.token
+	  const comments = await octokit.request(commentsEndpoint)
+	  console.log(comments)
 
-  // const octokit = new github.GitHub(github.token);
-
-  //   const { data: pullRequest } = await octokit.pulls.get({
-  //       owner: 'MichalBednarz',
-  //       repo: 'simplefeed',
-  //       pull_number: 123,
-  //       mediaType: {
-  //         format: 'diff'
-  //       }
-  //   });
-
-  //   console.log(pullRequest);
-} catch (error) {
-  core.setFailed(error.message);
-}
+	} catch (error) {
+	  core.setFailed(error.message);
+	}
+})()
