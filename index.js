9182e904ec995c330b21a59e64014367fac1b3dc
Michał Bednarz <mibed90@gmail.com>
2020-01-16 12:40:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const github = require('@actions/github'); // deps from toolkit or marketplace
 	  // console.log(commentsEndpoint) ok
 
 	// auth like this?
-	  console.log('log')
 	const myToken = core.getInput('myToken');
-	console.log(myToken)
-	const octokit = new github.Github(myToken)
+	const octokit = new github.Github(myToken);
 	  // const octokit = new github.GitHub(github.token, {
 	  // 	previews: ['comfort-fade-preview']
 	  // });
