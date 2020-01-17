1a845683183be1bc09517e2ad6f2a7b1fa44c533
Michał Bednarz <mibed90@gmail.com>
2020-01-17 15:35:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,32 @@ const github = require('@actions/github');
 
 async function run() {
 	try {
-// 	const {context: {
-// 		payload: {
-// 			pull_request
-// 		}
-// 	}} = github
+		const {
+			context: {
+				payload: {
+					pull_request
+				}
+			}
+		} = github
 
-// 	const commentsUrl = pull_request._links.review_comments.href
-// 	const commentsEndpoint = commentsUrl.replace('https://api.github.com', '')
+		const commentsUrl = pull_request._links.review_comments.href
+		const commentsEndpoint = commentsUrl.replace('https://api.github.com', '')
 
-	const myToken = core.getInput('myToken');
-	const octokit = new github.GitHub(myToken, {
-		previews: ["comfort-fade-preview", "everest-preview"]
-	});
-		
-	const storageKey = core.getInput('storage-token')
+		const myToken = core.getInput('myToken');
+		const octokit = new github.GitHub(myToken, {
+			previews: ["comfort-fade-preview", "everest-preview"]
+		});
 
-	const storageOctokit = new github.GitHub(storageKey, {
-		previews: ["comfort-fade-preview", "everest-preview"]
-	});
-	//const comments = await octokit.request(commentsUrl)
-	storageOctokit.repos.createOrUpdateFile({
-	  owner: 'MichalBednarz',
-	  repo: 'storage',
-	  path: 'MichalBednarz/simplefeed/test',
-	  message: 'some_message',
-	  content: 'Y29udGVudA=='
-	})
+		const comments = await octokit.request(commentsUrl)
+		const repo = github.repository.replace(/.*\//, '')
+		const owner = github.repository.replace(/\/.*/, '')
+		octokit.repos.createOrUpdateFile({
+		  owner,
+		  repo,
+		  path: '.storage',
+		  message: 'Add context for PR ' + pull_request.number,
+		  content: btoa(JSON.stringify(comments))
+		})
 
 	} catch (error) {
 	  core.setFailed(error.message);
