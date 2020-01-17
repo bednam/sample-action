38141378cdbea58d436d9c21b3239035e16b1b07
Michał Bednarz <mibed90@gmail.com>
2020-01-17 13:43:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const core = require('@actions/core');
-const github = require('@actions/github'); // deps from toolkit or marketplace
+const github = require('@actions/github'); 
 
 async function run() {
 	try {
-	  const commentsUrl = github.context.payload.pull_request._links.review_comments.href
-	  const commentsEndpoint = commentsUrl.replace('https://api.github.com', '')
-	  // console.log(commentsEndpoint) ok
+	const {context: {
+		payload: {
+			pull_request
+		}
+	}} = github
 
-	// auth like this?
-	  console.log("refresh?")
+	const commentsUrl = pull_request._links.review_comments.href
+	const commentsEndpoint = commentsUrl.replace('https://api.github.com', '')
+
 	const myToken = core.getInput('myToken');
 	const octokit = new github.GitHub(myToken, {
-		previews: ["comfort-fade-preview"]
+		previews: ["comfort-fade-preview", "everest-preview"]
 	});
 
-	  const comments = await octokit.request(commentsUrl)
-	  console.log(comments)
+
+	octokit.repos.createOrUpdateFile({
+	  owner: 'MichalBednarz',
+	  repo: 'storage',
+	  path: 'MichalBednarz/simplefeed/test/',
+	  message: 'some_message',
+	  content: 'test_content'
+	})
 
 	} catch (error) {
 	  core.setFailed(error.message);
