28bc247fabe79567dce85b67f34811c615a3ff6f
Michał Bednarz <mibed90@gmail.com>
2020-01-21 10:15:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const fs = require('fs')
 
 async function run() {
 	try {
-	  core.setOutput("comments", [{"path":"b6.txt","user":"MichalBednarz","body":"asdfa","line":3},{"path":"b6.txt","user":"MichalBednarz","body":"asdfads","line":5}])
+		core.setOutput("comments", [{"path":"b6.txt","user":"MichalBednarz","body":"asdfa","line":3},{"path":"b6.txt","user":"MichalBednarz","body":"asdfads","line":5}])
+		var stream = fs.createWriteStream("my_file.txt");
+		stream.once('open', function(fd) {
+		  stream.write("My first row\n");
+		  stream.write("My second row\n");
+		  stream.end();
+		});
 	} catch (error) {
 	  core.setFailed(error.message);
 	}
