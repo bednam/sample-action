5887b86eb94c669fef90accc0a0860309a67c929
Michał Bednarz <mibed90@gmail.com>
2020-02-03 12:47:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ const fs = require('fs')
 
 async function run() {
 	try {
-		console.log(github.secrets)
 		// read from snapshot
 		const mockSnapshot = { snapshot: "snapshot"}
-		// initialize octokit
-		// how to handle storage repo token?
 		const repoToken = core.getInput('repo-token')
 		const octokit = new github.GitHub(repoToken, {
 			previews: ["comfort-fade-preview", "everest-preview"]
 		})
 		// save snapshot to https://github.com/MichalBednarz/cb-storage
-		console.log('udpate')
 		try {
 			await octokit.repos.createDispatchEvent({
 			owner: "MichalBednarz",
