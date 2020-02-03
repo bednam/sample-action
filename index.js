194485033fdfe0c8ba8e850ed25f6b7a30425ae3
Michał Bednarz <mibed90@gmail.com>
2020-02-03 15:42:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const fs = require('fs')
 async function run() {
 	try {
 		// read from snapshot
-		const snapshot = fs.readdir('snapshot', (err, files) => err ? console.log(err) : files)
+		const snapshot = fs.readdir('snapshot', (err, files) => {
+			if(err) console.log(err)
+			
+			return files
+		})
 		console.log(JSON.stringify(snapshot))
 		const storageToken = core.getInput('storage-token')
 		const octokit = new github.GitHub(storageToken, {
