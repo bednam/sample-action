044324e1270642b37b47c730c784dd674a6c735d
Michał Bednarz <mibed90@gmail.com>
2020-02-06 11:09:34 +0100

    Migrate to graphql

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const { graphql } = require("@octokit/graphql");
 const fs = require('fs')
 
 async function run() {
 	try {
-		const comments = [{"path":"b6.txt","user":"MichalBednarz","body":"asdfa","line":3},{"path":"b6.txt","user":"MichalBednarz","body":"asdfads","line":5}]
+		// const {
+		// 		context: {
+		// 			payload: {
+		// 				number
+		// 			}
+		// 		}
+		// 	} = github
+
+		const repoToken = core.getInput('repo-token')
+
+		const graphqlWithAuth = graphql.defaults({
+		  headers: {
+		    authorization: `token ${repoToken}`
+		  },
+		  previews: ["comfort-fade-preview"]
+		})
+
+		const name = github.repository.replace(/.+\//, '')
+		const owner = github.repository.replace(\/.+, '')
+
+		const { 
+			repository: {
+				pullRequests: {
+					nodes
+				}
+			} 
+		} = await graphqlWithAuth(`
+		  {
+		    repository(owner: ${owner}, name: ${name}) {
+		    	pullRequest(number: 7) { 
+		    	  	reviewThreads(first:10){
+		    	      nodes {
+		    	        comments(first:50) {
+		    	          nodes {
+		    	            body
+		    	            author {
+		    	              login
+		    	            }
+		    	            originalPosition
+		    	            path
+		    	            outdated
+		    	          }
+		    	        }
+		    	      }
+		    	    }
+		    	}
+		  }
+		`)
+
+		const comments = x.nodes
+			.flatMap(pr => pr.reviewThreads.nodes)
+			.flatMap(thread => thread.comments.nodes)
+			.filter(comment => !comment.outdated)
+
 		const filePath = 'ReviewComments.json'
 		fs.writeFile(filePath, JSON.stringify(comments), (err) => {
 		  if(err) throw err
